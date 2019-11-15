820142dfa2b7df71fa84dd82eb2d20937fab5d7a
allensam88 <54912919+allensam88@users.noreply.github.com>
2019-11-15 12:03:44 -0800

    got add functionality for stretch goal!

--- client/src/components/AddColor.js
+++ client/src/components/AddColor.js
@@ -5,42 +5,26 @@ const AddColor = props => {
     const [newColor, setNewColor] = useState({
         id: '',
         color: '',
-        code: ''
+        code: {hex: ''}
     });
     
     const handleChange = (event) => {
-        event.persist();
-        // // let value = event.target.value;
-        // if (event.target.name === 'code') {
-        //     let value = JSON.parse(`{ 'hex': ${event.target.value} }`);
-        //     setNewColor({ ...newColor, [event.target.name]: value });
-        // var params = `[{"hex": ${event.target.name}}]`;
-        // obj = JSON.parse(params);
-        // console.log(obj)
-        // }
-        
         setNewColor({ ...newColor, [event.target.name]: event.target.value });
+    }
+
+    const handleHexChange = (event) => {
+        setNewColor({ ...newColor, code: {[event.target.name]: event.target.value} });
     }
 
     const submitForm = event => {
         event.preventDefault();
-        const addColor = {
-            id: Date.now(),
-            color: newColor.color,
-            code: newColor.code,
-        };
         AxiosWithAuth()
-            .post('/colors', addColor)
+            .post('/colors', newColor)
             .then(res => {
                 props.history.push('/bubble-page')
-                alert(`Successfully added ${addColor.color}`);
+                alert(`Successfully added ${newColor.color}`);
             })
             .catch(err => console.log(err));
-        setNewColor({
-            id: '',
-            color: '',
-            code: ''
-        });
     };
 
     return (
@@ -56,9 +40,9 @@ const AddColor = props => {
                 />
                 <input
                     type='text'
-                    name='code'
-                    value={newColor.code}
-                    onChange={handleChange}
+                    name='hex'
+                    value={newColor.code.hex}
+                    onChange={handleHexChange}
                     placeholder='code'
                     autoComplete='off'
                 />
